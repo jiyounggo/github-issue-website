6dbd891071e5d1793309235940d2f605ae19f1f4
ezn6 <kcourtney@naver.com>
2022-09-14 07:30:15 +0900

    Feat: 추가 라우터
    이슈 상세화면의 라우터를 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { ROUTE } from './common/utils/constant';
+import IssueDetail from './pages/IssueDetail/IssueDetail';
 import IssueList from './pages/IssueList/IssueList';
 import NotFound from './pages/NotFound/NotFound';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path={ROUTE.MAIN} element={<IssueList />} />
+      <Route path={ROUTE.DETAIL_ISSUE} element={<IssueDetail />} />
       <Route path={ROUTE.NOT_FOUND} element={<NotFound />} />
     </Routes>
   );
