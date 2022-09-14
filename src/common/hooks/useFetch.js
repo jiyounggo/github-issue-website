fabe0e5d67af6c854394d2648217a5fc4f534e08
cssong <a1373052011@gmail.com>
2022-09-14 20:06:48 +0900

    Feat: 변경 useFetch
    useFetch를 목록, 상세화면에서 모두 사용할 수 있도록 변경
    - setData 시 기존 state에 신규 state를 합치는 것이 아닌 신규 state만 반환하도록 수정
    - 기존과 신규 state를 병합하는 작업은 IssueApi 컴포넌트에서 작업하도록 변경
    - 파라미터를 page 에서 객체(method, url, params)로 변경
    - 파라미터 변경으로 인한 useCallback 의존성 변경
    - 의존성에 추가된 params는 상위 단계에서 전달할 때 useMemo를 통해 값이 변경하면 객체도 변경되어 전달하도록 변경
    - 추후 병합 작업을 위해 useFetch_v2로 임시 생성

--- src/common/hooks/useFetch.js
+++ src/common/hooks/useFetch.js
@@ -31,3 +31,38 @@ export default function useFetch(page) {
 
   return { isLoaded, error, list };
 }
+const api = axios.create({
+  baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+  headers: {
+    Accept: 'application/vnd.github+json',
+    Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+  },
+});
+
+export function useFetch_v2({ method = 'get', url, params }) {
+  const [isLoaded, setIsLoad] = useState(true);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState();
+
+  const sendQuery = useCallback(async () => {
+    try {
+      setIsLoad(true);
+      setError(false);
+      const res = await api({
+        method,
+        url,
+        params,
+      });
+      setData(res.data);
+      setIsLoad(false);
+    } catch (err) {
+      setError(err);
+    }
+  }, [params]);
+
+  useEffect(() => {
+    sendQuery();
+  }, [sendQuery]);
+
+  return { isLoaded, error, data };
+}
