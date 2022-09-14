a05775921a5c871990bc5fa03da45989665662bd
ted-jv <wkejrdldls@gmail.com>
2022-09-15 07:40:18 +0900

    Fix: Main.jsx key 중복 콘솔 에러 map 함수에서 if문 부분의 개별 key를 안 넣어줬던 문제

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -15,7 +15,7 @@ const Main = () => {
       {IssueData?.map((value, index) => {
         if (index === 4) {
           return (
-            <a href="https://thingsflow.com/ko/home">
+            <a key="a12345" href="https://thingsflow.com/ko/home">
               <AdImg
                 src="https://younuk.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fbf0a0656-3146-4e9b-8739-7bca3d0d2cb4%2F%25E1%2584%2584%25E1%2585%25B5%25E1%2586%25BC%25E1%2584%2589%25E1%2585%25B3%25E1%2584%2591%25E1%2585%25B3%25E1%2586%25AF%25E1%2584%2585%25E1%2585%25A9%25E1%2584%258B%25E1%2585%25AE_%25E1%2584%2585%25E1%2585%25A9%25E1%2584%2580%25E1%2585%25A9_%25E1%2584%2580%25E1%2585%25B5%25E1%2584%2587%25E1%2585%25A9%25E1%2586%25AB%25E1%2584%2592%25E1%2585%25A7%25E1%2586%25BC.png?table=block&id=23d7e96e-0dbc-4ba3-9e41-c0f22a5ba926&spaceId=72b256b1-ae08-4e70-bb6c-f9c3cad5a793&width=2000&userId=&cache=v2"
                 alt="광고"
