2bb3cfac08d61ec4ef43fdb0a418c656b5f02ef0
Sujin Choi <hissujinc@gmail.com>
2022-09-14 18:38:49 +0900

    Refactor: 정리 API
    공통 컴포넌트로 빠질수있는 API 정리

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,18 @@
-const baseUrl = process.env.REACT_APP_SERVER_URL;
+import axios from 'axios';
 
-const get = async endpoint => {
-  const url = baseUrl + endpoint;
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error(`${res.status.toString()} Error 인한 요청 실패!`);
-  }
-  const result = await res.json();
+const API_URL = `${process.env.REACT_APP_API_URL}`;
+const OWNER = `${process.env.REACT_APP_OWNER}`;
+const REPO = `${process.env.REACT_APP_REPO}`;
+const GH_TOKEN = `${process.env.REACT_APP_TOKEN}`;
 
-  return result;
+const api = axios.create({
+  baseURL: `${API_URL}/${OWNER}/${REPO}`,
+  headers: { Accept: 'application/vnd.github+json', Authorization: `Bearer ${GH_TOKEN}` },
+});
+
+const getListByIssues = async query => {
+  const response = await api.get(``, { params: { page: query } });
+  return response;
 };
 
-export { get };
+export { getListByIssues };
