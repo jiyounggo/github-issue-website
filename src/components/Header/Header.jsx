a2d77065fb7b2d62e7f2626cb23900651233c464
Malza0408 <parkjh04080@gmail.com>
2022-09-15 00:06:26 +0900

    Feat: 추가 Header component
    공통으로 사용 할 Header component 생성

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header as Container, Title } from './Header.style';
+
+function Header() {
+  return (
+    <Container>
+      <Title>Angular / Angular-cli</Title>
+    </Container>
+  );
+}
+
+export default Header;
