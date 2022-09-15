6f65ebda70d44be91bb6155e7382615d72b1fcdb
ezn6 <kcourtney@naver.com>
2022-09-15 10:25:23 +0900

    Refactor: 수정 api 헤더
    access token을 넣지 않도록 수정

--- src/api/api.js
+++ src/api/api.js
@@ -3,11 +3,10 @@ import axios from 'axios';
 const API_URL = `${process.env.REACT_APP_API_URL}`;
 const OWNER = `${process.env.REACT_APP_OWNER}`;
 const REPO = `${process.env.REACT_APP_REPO}`;
-const GH_TOKEN = `${process.env.REACT_APP_TOKEN}`;
 
 const api = axios.create({
   baseURL: `${API_URL}/${OWNER}/${REPO}`,
-  headers: { Accept: 'application/vnd.github+json', Authorization: `Bearer ${GH_TOKEN}` },
+  headers: { Accept: 'application/vnd.github+json' },
 });
 
 const getListByIssues = async (state, sort, page) => {
